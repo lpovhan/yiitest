99bb5f4a8ab10f1a1c5444761ae02dd577948f03
Lyubov Povkhan <lyubov.povhan@gmail.com>
2026-03-17 09:31:00 +0100

    fixed mysql connection

--- app/config/db.php
+++ app/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host='.$_ENV['DB_HOST'].';dbname=' . $_ENV['DB_DATABASE'],
+    'dsn' => 'mysql:host='.$_ENV['DB_HOST'].';port=3306;dbname=' . $_ENV['DB_DATABASE'],
     'username' => $_ENV['DB_USERNAME'],
     'password' => $_ENV['DB_PASSWORD'],
     'charset' => 'utf8',
